c25f3792e467e9687e9fcf8de781a1c2a0267eea
hakimLyon <beatsjordy@gmail.com>
2021-01-21 15:33:06 -0500

    Updates

---
+++ precache-manifest.6d30e044d2030361dce4e19d6f74735f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4f6ee7f4489d36db01bb3e78b42b56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a139f267d4dc9cab552d",
+    "url": "/static/css/main.d8e975bd.chunk.css"
+  },
+  {
+    "revision": "dc02afbb6f89ef6534ae",
+    "url": "/static/js/2.baaa6981.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.baaa6981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a139f267d4dc9cab552d",
+    "url": "/static/js/main.f1ed218e.chunk.js"
+  },
+  {
+    "revision": "e913026706bbc384d512",
+    "url": "/static/js/runtime-main.a364cf6e.js"
+  },
+  {
+    "revision": "1ed29ac553c41e3eaa01667259709485",
+    "url": "/static/media/logo1.1ed29ac5.svg"
+  },
+  {
+    "revision": "23982a999a5743a2ea178564a025a6ed",
+    "url": "/static/media/logo2.23982a99.svg"
+  },
+  {
+    "revision": "b2e4ca31fff821dc5a80786fc04c3560",
+    "url": "/static/media/profile.b2e4ca31.jpg"
+  }
+]);
